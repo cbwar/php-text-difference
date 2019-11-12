eb40ef2f2c1e37438b87a6afe393d8bc163d3162
Raphael Lisch <raphael.lisch@gestour.com>
2019-11-12 09:48:55 +0100

    fix strict comparisons

--- src/Renderer/Text/Unified.php
+++ src/Renderer/Text/Unified.php
@@ -67,18 +67,18 @@ class Unified extends AbstractRenderer
                 $i2 = -1;
             }
 
-            $diff .= '@@ -' . ($i1 + 1) . ',' . ($i2 - $i1) . ' +' . ($j1 + 1) . ',' . ($j2 - $j1) . " @@" . PHP_EOL;
+            $diff .= '@@ -' . ($i1 + 1) . ',' . ($i2 - $i1) . ' +' . ($j1 + 1) . ',' . ($j2 - $j1) . ' @@' . PHP_EOL;
             foreach ($group as $code) {
                 list($tag, $i1, $i2, $j1, $j2) = $code;
-                if ($tag == 'equal') {
-                    $diff .= ' ' . implode(PHP_EOL . " ", $this->diff->GetA($i1, $i2)) . PHP_EOL;
+                if ($tag === 'equal') {
+                    $diff .= ' ' . implode(PHP_EOL . ' ', $this->diff->GetA($i1, $i2)) . PHP_EOL;
                 } else {
-                    if ($tag == 'replace' || $tag == 'delete') {
-                        $diff .= '-' . implode(PHP_EOL . "-", $this->diff->GetA($i1, $i2)) . PHP_EOL;
+                    if ($tag === 'replace' || $tag === 'delete') {
+                        $diff .= '-' . implode(PHP_EOL . '-', $this->diff->GetA($i1, $i2)) . PHP_EOL;
                     }
 
-                    if ($tag == 'replace' || $tag == 'insert') {
-                        $diff .= '+' . implode(PHP_EOL . "+", $this->diff->GetB($j1, $j2)) . PHP_EOL;
+                    if ($tag === 'replace' || $tag === 'insert') {
+                        $diff .= '+' . implode(PHP_EOL . '+', $this->diff->GetB($j1, $j2)) . PHP_EOL;
                     }
                 }
             }
